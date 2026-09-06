a87c9dbedce9c88c0447d28cf7fee8ea6ccf14ed
SabriR <russellsabrinam@gmail.com>
2026-09-06 13:10:58 -0300

    Fixed favorites grid

--- src/components/FavoriteCard/FavoriteCard.jsx
+++ src/components/FavoriteCard/FavoriteCard.jsx
@@ -7,24 +7,24 @@ export default function FavoriteCard({ item, onRemove }) {
 
   return (
     <article className="favorite-card">
-      <Link to={`/${entity}/${id}`} className="favorite-card-image-link">
-        {image ? (
-          <img src={image} alt={`Imagen de ${name}`} className="favorite-card-image" />
-        ) : (
-          <div className="favorite-card-image-placeholder">?</div>
-        )}
-      </Link>
-
+      <div className='favorite-card-media'>
+        <Link to={`/${entity}/${id}`} className="favorite-card-image-link">
+          {image ? (
+            <img src={image} alt={`Imagen de ${name}`} className="favorite-card-image" />
+          ) : (
+            <div className="favorite-card-image-placeholder">?</div>
+          )}
+        </Link>
+        <span className="favorite-card-seal" title="Prioridad">
+          {priority}
+        </span>
+      </div>
       <div className="favorite-card-body">
         <div className="favorite-card-header">
           <Link to={`/${entity}/${id}`} className="favorite-card-title-link">
             <h3 className="favorite-card-title">{name}</h3>
           </Link>
 
-          {/* Prioridad como sello de lacre */}
-          <span className="favorite-card-seal" title="Prioridad">
-            {priority}
-          </span>
         </div>
 
         <span className="favorite-card-tag">{category}</span>
